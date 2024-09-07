9ace9f4c4c2aca3e3d5f57d3624a58148525d58b
Will <will.stephano@gmail.com>
2024-09-07 10:57:10 -0300

    Implementação de permissao de uso de geolocalização

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,12 +1,17 @@
-import Constants from "expo-constants";
-import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import { IconButton, useTheme } from "react-native-paper";
-import { Usuario } from "../../models/Usuario";
-import { UsuarioService } from "../../services/usuario.service";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
-import { RootStackParamList } from "../../navigation/AppNavigator"; // Ajuste o caminho conforme necessário
+import Constants from "expo-constants";
+import {
+  getCurrentPositionAsync,
+  LocationObject,
+  requestForegroundPermissionsAsync,
+} from "expo-location";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { IconButton, useTheme } from "react-native-paper";
+import { Usuario } from "../../models/Usuario";
+import { RootStackParamList } from "../../navigation/AppNavigator";
+import { UsuarioService } from "../../services/usuario.service";
 
 type HeaderNavigationProp = StackNavigationProp<RootStackParamList, "Perfil">;
 
@@ -14,7 +19,8 @@ export function Header() {
   const { colors } = useTheme();
   const statusBarHeight = Constants.statusBarHeight;
   const [usuario, setUsuario] = useState<Usuario | null>(null);
-  const navigation = useNavigation<HeaderNavigationProp>(); // Definindo o tipo de navegação
+  const [location, setLocation] = useState<LocationObject | null>(null);
+  const navigation = useNavigation<HeaderNavigationProp>();
 
   useEffect(() => {
     async function fetchUsuario() {
@@ -35,7 +41,24 @@ export function Header() {
   // Função para redirecionar para a tela de perfil
   const handleUserPress = () => {
     if (usuario) {
-      navigation.navigate("Perfil"); // Usando o tipo de navegação correto
+      navigation.navigate("Perfil");
+    }
+  };
+
+  // Função para solicitar permissão de localização
+  const requestLocationPermission = async () => {
+    try {
+      const { granted } = await requestForegroundPermissionsAsync();
+
+      if (granted) {
+        const currentPosition = await getCurrentPositionAsync();
+        setLocation(currentPosition);
+        console.log("Localização atual: ", currentPosition);
+      } else {
+        console.log("Permissão de localização negada");
+      }
+    } catch (error) {
+      console.error("Erro ao obter localização:", error);
     }
   };
 
@@ -46,7 +69,7 @@ export function Header() {
         <IconButton
           icon="map-marker"
           size={20}
-          onPress={() => console.log("Map Pin Pressed")}
+          onPress={requestLocationPermission} // Chama a função ao clicar
           iconColor={"red"}
         />
       </View>
