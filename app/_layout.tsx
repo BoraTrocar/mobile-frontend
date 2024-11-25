f6010bbe4752cb308f82b68fbeb2853077bf36c3
PatriciaLais <patricialais@hotmail.com>
2024-11-25 19:26:35 -0300

    Correção de erro de compatibilidade em dependencias

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,16 @@
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
+import { NavigationContainer } from "@react-navigation/native";
+import { RaioProvider } from "@/RaioContext";
+import { Provider as PaperProvider } from "react-native-paper";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{
-          headerShown: false,
-        }}
-      />
-    </Stack>
+    <RaioProvider>
+      <PaperProvider>
+        <NavigationContainer>
+          <Slot />
+        </NavigationContainer>
+      </PaperProvider>
+    </RaioProvider>
   );
 }
