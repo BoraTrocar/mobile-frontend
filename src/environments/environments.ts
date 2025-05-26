d08da379fe2a9cbb6552f15ccef1326ed7acec45
Flávio Almeida <112271774+FlavioAlmeidaa@users.noreply.github.com>
2025-05-26 16:53:55 -0300

    mudança de endereço

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   //apiUrl: "http://192.168.1.135:8090",
 
-  apiUrl: "https://boratrocar.net:8090",
+  apiUrl: "https://boratrocar.company:8090",
 
   //NAO pode usar localhost demorei 4hrs para descobrir isso. Caralho.
 
